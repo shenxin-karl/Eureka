00d96fe9ed22c40409e6f9f9cd6e0f65b2d5a43b
沈鑫 <1683137709@qq.com>
2022-12-13 01:03:11 +0800

    cmake 调用 flex bison

--- EffectCompiler/Main.cpp
+++ EffectCompiler/Main.cpp
@@ -1,5 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cassert>
 #include <iostream>
 
+
+extern FILE *yyin;
+extern int yyparse(std::unique_ptr<std::string> &ast);
+
 int main() {
+	yyin = fopen("Main.txt", "r");
+	assert(yyin);
+
+	// 调用 parser 函数, parser 函数会进一步调用 lexer 解析输入文件的
+	std::unique_ptr<std::string> ast;
+	auto ret = yyparse(ast);
+	assert(!ret);
+
+	// 输出解析得到的 AST, 其实就是个字符串
+	std::cout << *ast << std::endl;
 	return 0;
 }
